90de13a30428d849d64353df61099019224cd2c6
Francisco de Jesus Dias André <francisco.andre@gesprin.co.ao>
2019-02-03 09:52:35 +0100

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,60 @@
+import { CriarfaturaComponent } from './faturas/add/criarfatura.component';
+import { ProdutoslistaComponent } from './produtos/produtoslista/produtoslista.component';
+
+import { ClienteslistaComponent } from './clientes/clienteslista/clienteslista.component';
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PainelComponent } from './painel/painel.component';
+import { ListardetalhesclienteComponent } from './clientes/detalhes/listardetalhescliente.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { CriarprodutoComponent } from './produtos/criarproduto/criarproduto.component';
+import { PagamentosComponent } from './pagamentos/pagamentos.component';
+import { FaturaComponent } from './faturas/fatura/fatura.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'painel',
+  pathMatch: 'full'
+},
+{
+  path: 'painel',
+  component: PainelComponent
+},
+{
+  path: 'faturaprint',
+  component: FaturaComponent
+},
+{
+  path: 'clientes',
+  component: ClienteslistaComponent
+},
+{
+  path: 'pagamentos',
+  component: PagamentosComponent
+},
+
+{
+  path: 'produtos',
+  component: ProdutosComponent,
+  children: [
+    { path: '', redirectTo: 'listar', pathMatch: 'full' },
+    { path: 'listar', component: ProdutoslistaComponent },
+    {path: 'criar', component: CriarprodutoComponent}
+  ]
+},
+{
+  path: 'faturas',
+  component: CriarfaturaComponent
+},
+{ path: 'detalhescliente/:id', component: ListardetalhesclienteComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,85 @@
+import { ProdutoslistaComponent } from './produtos/produtoslista/produtoslista.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavegacaoComponent } from './navegacao/navegacao.component';
+import { LayoutModule } from '@angular/cdk/layout';
+// tslint:disable-next-line:max-line-length
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatDialogModule, MatCardModule, MatSelectModule, MatInputModule } from '@angular/material';
+import { PainelComponent } from './painel/painel.component';
+
+import { ClienteService } from './servicos/cliente.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { ClienteslistaComponent } from './clientes/clienteslista/clienteslista.component';
+import { CriarclienteComponent } from './clientes/add/criarcliente.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CriarfaturaComponent } from './faturas/add/criarfatura.component';
+import { ListardetalhesclienteComponent } from './clientes/detalhes/listardetalhescliente.component';
+import { CriarprodutoComponent } from './produtos/criarproduto/criarproduto.component';
+import { ToastrModule } from 'ngx-toastr';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { PagamentosComponent } from './pagamentos/pagamentos.component';
+import { FaturaComponent } from './faturas/fatura/fatura.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacaoComponent,
+    PainelComponent,
+    ClienteslistaComponent,
+    CriarclienteComponent,
+    CriarfaturaComponent,
+    ProdutoslistaComponent,
+    ListardetalhesclienteComponent,
+    CriarprodutoComponent,
+    ProdutosComponent,
+    PagamentosComponent,
+    FaturaComponent,
+
+  ],
+  entryComponents: [
+    CriarclienteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    ToastrModule.forRoot()
+
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ClienteService],
+  bootstrap: [NavegacaoComponent]
 })
 export class AppModule { }
